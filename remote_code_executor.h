a02266fffa7a933979eca26c54e4013f08fb6aa3
myanwc <anton.kostyrev@yandex.ru>
2026-02-04 15:57:51 +0300

    Add helper for converting wide strings to std strings

--- remote_code_executor.h
+++ remote_code_executor.h
@@ -18,6 +18,21 @@ public:
 
     }
 
+    static std::string WideCharToString(const WCHAR* wstr) {
+        if (!wstr) {
+            return "";
+        }
+
+        int size_needed = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, nullptr, 0, nullptr, nullptr);
+        std::string str(size_needed, 0);
+        WideCharToMultiByte(CP_UTF8, 0, wstr, -1, &str[0], size_needed, nullptr, nullptr);
+
+        if (!str.empty() && str.back() == '\0') {
+            str.pop_back();
+        }
+        return str;
+    }
+
 private:
 
 }

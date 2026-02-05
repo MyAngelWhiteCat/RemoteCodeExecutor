58c5c7b3059b89cc3f37c0532d5ec317595d89b8
myanwc <anton.kostyrev@yandex.ru>
2026-02-05 10:30:49 +0300

    Fix main.cpp for console using

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,21 @@
 #include <string>
 #include <exception>
 
+#include "domain.h"
 #include "remote_code_executor.h"
 
-int main(int argc, wchar_t** argv) {
-    if (argc != 3) {
-        std::wcout << "Usage: " << argv[0] << " <dll> <process>" << std::endl;
-    }
-    std::filesystem::path dll_path(argv[1]);
+
+int main(int argc, char** argv) {
+    std::filesystem::path dll_path(std::filesystem::current_path() / argv[1]);
     if (!std::filesystem::exists(dll_path)) {
-        std::wcout << argv[1] << " is not exist" << std::endl;
+        std::wcout << "DLL do not exist " << dll_path.wstring() << std::endl;
         return 1;
     }
 
     try {
-        RemoteCodeExecutor::InjectDLL(dll_path.wstring(), argv[2]);
-        std::wcout << "Successfully injected " << argv[1] << " to " << argv[2] << std::endl;
+        RemoteCodeExecutor::InjectDLL(dll_path.wstring(),
+            domain::StringToWideChar(argv[2]));
+        std::wcout << "Successfully injected " << " to " << argv[2] << std::endl;
     }
     catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
